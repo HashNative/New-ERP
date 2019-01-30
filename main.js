4ff3b3426c021fe284423929c3152dddb235829a
SCArthi <arthika19@gmail.com>
2019-01-30 16:45:45 +0530

    Second task commit

--- main.js
+++ main.js
@@ -9,7 +9,8 @@ const url = require("url");
 let win;
 
 function createWindow() {
-    win = new BrowserWindow({ maxWidth: 800, maxHeight: 500 });
+    win = new BrowserWindow();
+    win.maximize();
     win.loadURL(url.format({
         pathname: path.join(__dirname, 'index.html'),
         protocol: 'file',
